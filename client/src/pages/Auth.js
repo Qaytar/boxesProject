e7cc6280b7a4071b49a9f24e7054bf1f8f4a1d3e
Your Name01~ <you@example.com>
2023-06-21 18:50:49 +0200

    added Google Auth

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,8 +1,30 @@
+import googleButton from '../assets/btn_google_signin_light_normal_web.png'
+
+function navigate(url) {
+    window.location.href = url;
+}
+
+async function auth() {
+    const response = await fetch('http://localhost:5000/auth/google', { method: 'post' });
+
+    const data = await response.json();
+    console.log(data);
+    navigate(data.url);
+
+}
+
+
 function Auth() {
+
+
     return (
-        <h1>
-            Login/Registration Page
-        </h1>
+        <>
+            <h3>Google OAuth!</h3>
+
+            <button className="btn-auth" type="button" onClick={() => auth()}>
+                <img className="btn-auth-img" src={googleButton} alt='google sign in' />
+            </button>
+        </>
     )
 }
 
