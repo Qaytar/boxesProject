57d777bdeed7c25183fe85561d4514dce0a441e2
Your Name01~ <you@example.com>
2023-06-28 17:24:00 +0200

    added two new contexts, for selectedBoxes and to keep track of the lifeBoardData

--- server/dbRoutes.js
+++ server/dbRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./models');
+const jwt = require('jsonwebtoken');
 
 // Function to create an empty lifeBoard
 function createEmptyLifeBoard() {
@@ -19,21 +20,26 @@ function createEmptyLifeBoard() {
 /*Replies with either empty lifeBoard or the one stored in db for that specific user*/
 router.get('/getLifeBoard', async (req, res) => {
     try {
-        const { user } = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+        // Check if a token exists
+        if (req.cookies && req.cookies.token) {
+            const { user } = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
 
-        // If there's a user, try to get their lifeBoard from the DB
-        if (user) {
-            const userData = await User.findOne({ id: user.id });
-            return res.json(userData.lifeBoard);
-        } else {
-            // No user, return an empty lifeBoard
-            return res.json(createEmptyLifeBoard());
+            // If there's a user, try to get their lifeBoard from the DB
+            if (user) {
+                const userData = await User.findOne({ id: user.id });
+                return res.json(userData ? userData.lifeBoard : createEmptyLifeBoard());
+            }
         }
+
+        // No user (or no token), return an empty lifeBoard
+        return res.json(createEmptyLifeBoard());
+
     } catch (error) {
         console.error("Error getting lifeBoard:", error);
         res.status(500).json({ error: 'Failed to get lifeBoard' });
     }
 });
+
 
 module.exports = router;
 
