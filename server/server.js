841c4229ff8776873d594fbdf0fc1471ebdbd115
Your Name01~ <you@example.com>
2023-11-01 13:47:38 +0100

    debugging favicons

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-mongoose.set('debug', true);
+// mongoose.set('debug', true);
 
 const app = express();
 const cors = require('cors');
