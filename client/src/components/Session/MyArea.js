e5334ba344e67f6334dbc0e92986674143a9863d
Qaytar <cescnunes@gmail.com>
2024-01-22 11:40:10 +0100

    created confirmation popup for the delete all btn

--- client/src/components/Session/MyArea.js
+++ client/src/components/Session/MyArea.js
@@ -10,13 +10,20 @@ import styles from "./MyArea.module.css"
 import { AuthContext } from '../../contextsAndHooks/authContext';
 import { LifeBoardDataContext } from '../../contextsAndHooks/lifeBoardDataContext';
 import { saveBirthDate } from '../../helpers/databaseOpsHelper';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 function MyArea(props) {
     const { setUser } = useContext(AuthContext);
     const { birthDate, setBirthDate } = useContext(LifeBoardDataContext)
     const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
-    console.log('birthDate',birthDate)
+    
+    const [deleteConfirmationPopup, setDeleteConfirmationPopup] = useState('closed');
+
+    // Handler for delete-data btn
+    function deleteDataPopupToggler(){
+        setDeleteConfirmationPopup(deleteConfirmationPopup ==='closed' ? 'open' : 'closed')
+    }
+
     // Function to handle date change in the form
     function handleDateChange(event) {
         //console.log('handleDateChange function is called')
@@ -54,7 +61,7 @@ function MyArea(props) {
             <div className={styles.modal}>
                 <div className={styles.dateWrapper}>
                     <p className={styles.title}>Set yor birth date and click save</p>
-                    <p className={styles.tutorialLocation}>If you need help to navigate the app, you can find the tutorial on the top right corner of the screen</p>
+                    <p className={styles.subTitle}>If you need help to navigate the app, you can find the tutorial on the top right corner of the screen</p>
                     <input
                         type="date"
                         value={birthDate ? new Date(birthDate).toISOString().substring(0, 10) : ""}
@@ -62,15 +69,21 @@ function MyArea(props) {
                         id="input"
                     />
                     <button className={styles.mainButton} onClick={() => { saveBirthDate(birthDate); props.onModalClick(); }}>Save</button>
-                </div>
-                <p className={styles.title}>Others</p>
+                </div>                
                 <div className={`${styles.othersWrapper}`}>
                 <span className={`${styles.others}`} onClick={logOut}>Logout and back to Homepage</span>
                 <span className={`${styles.others}`} >Privacy Policy</span>                
                 <span className={`${styles.others}`} >Delete account and all data</span>
-                </div>
-                
+                </div>                
             </div>
+            {deleteConfirmationPopup ? (<div className={styles.deleteConfirmationPopup}>
+                <p className={styles.title}>Are you sure?</p>
+                <p className={styles.subTitle}>This will permanently and irreversibly delete all your data and work you have done on your Life Calendar</p>
+                <p className={styles.subTitle}>Will also cause and inmdiate log out and redirect to the home page</p>
+                <span className={`${styles.confirmationOptions}`}>Yes, delete it all</span>
+                <span className={`${styles.confirmationOptions}`} onClick={deleteDataPopupToggler}>Cancel</span>
+            </div>) : null}
+            
         </div>
     )
 }
